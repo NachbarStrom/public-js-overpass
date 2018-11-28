d4862ba78c457317b2337f63b2afc3d730b53ef7
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2018-11-28 18:36:05 +0100

    Update setup script

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -10,5 +10,4 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 sudo apt-get update && sudo apt-get install yarn
 
 # Install node dependencies
-yarn
-
+yarn install
